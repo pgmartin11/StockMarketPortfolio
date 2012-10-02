463396a96c1ba6b1f61cbed724c3e572c15348a6
spencer.marks <spencer.marks@gmail.com>
2012-10-02 19:38:44 -0400

    spencer shows allan how to add a file. (and also check in his notes changes)

---
+++ src/test/java/edu/uml/project90308/presistence/AllanDeleteMe.java
@@ -0,0 +1,11 @@
+package edu.uml.project90308.presistence;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sam
+ * Date: 10/2/12
+ * Time: 7:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AllanDeleteMe {
+}
